9b9d6be66571b50371502454867562b9648c6253
Alex Dou <anhtudo97@gmail.com>
2025-05-14 01:30:39 +0700

    feat: update sections

---
+++ src/components/Circle.tsx
@@ -0,0 +1,18 @@
+import React, { type ComponentPropsWithoutRef } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+const Circle = ({ className, children }: ComponentPropsWithoutRef<'div'>) => {
+  return (
+    <div
+      className={twMerge(
+        'bg-zinc-900 size-[240px] inline-flex items-center justify-center rounded-full outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10',
+        'left-[200px] top-[270px]',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Circle;
