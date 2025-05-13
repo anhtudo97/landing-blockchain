05fe0e7b1638738f287c70b8364a58c666cea820
Alex Dou <anhtudo97@gmail.com>
2025-05-14 02:22:30 +0700

    feat: features cards

---
+++ src/sections/FeaturesCards.tsx
@@ -0,0 +1,130 @@
+import { twMerge } from 'tailwind-merge';
+
+const data = [
+  {
+    image: '/assets/images/pill.png', // Replace with your image path
+    title: 'Revolutionary Blockchain API',
+    description:
+      'Effortlessly integrate with our advanced blockchain API to access real-time data and streamline your application development process.',
+    color: 'fuchsia',
+  },
+  {
+    image: '/assets/images/cuboid.png', // Replace with your image path
+    title: 'Decentralized Blockchain API',
+    description:
+      'Empower your blockchain development with our cutting-edge API solution. Seamlessly integrate and harness the power of blockchain technology to build innovative and scalable applications.',
+    color: 'lime',
+  },
+  {
+    image: '/assets/images/cone.png', // Replace with your image path
+    title: 'Next Generation Blockchain API',
+    description:
+      'Unlock the potential of blockchain technology with our cutting-edge API solution. Seamlessly integrate and harness the power of blockchain to build innovative and scalable applications.',
+    color: 'cyan',
+  },
+  {
+    image: '/assets/images/icosahedron.png', // Replace with your image path
+    title: 'Seamless Integration with Blockchain API',
+    description:
+      'Integrate with our cutting-edge blockchain API to streamline your blockchain development process and unlock the full potential of blockchain technology.',
+    color: 'violet',
+  },
+];
+
+const FeaturesCards = () => {
+  return (
+    <section className="py-24 overflow-x-clip md:-mt-28">
+      <div className="container">
+        <h2 className="font-heading font-black text-4xl md:text-5xl text-center">
+          Discover the future of blockchain technology with our cutting-edge
+          solutions.
+        </h2>
+
+        <div className="mt-36 md:mt-48 flex overflow-x-scroll">
+          <div className="flex flex-none gap-8">
+            {data.map(({ color, description, image, title }, i) => {
+              return (
+                <div
+                  key={title}
+                  className="relative z-0 p-8 md:p-10 max-w-sm md:max-w-md group"
+                >
+                  <div
+                    className={twMerge(
+                      'absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 blur-lg opacity-0 group-hover:opacity-100 transition duration-300',
+                      color === 'lime' && 'bg-lime-500',
+                      color === 'cyan' && 'bg-cyan-500',
+                      color === 'violet' && 'bg-violet-500',
+                    )}
+                  ></div>
+                  <div
+                    className={twMerge(
+                      'absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 group-hover:bg-fuchsia-400 transition duration-300',
+                      color === 'lime' && 'bg-lime-500 group-hover:bg-lime-400',
+                      color === 'cyan' && 'bg-cyan-500 group-hover:bg-cyan-400',
+                      color === 'violet' &&
+                        'bg-violet-500 group-hover:bg-violet-400',
+                    )}
+                  ></div>
+                  <div className="absolute inset-0 bg-zinc-800 -z-10 rounded-2xl [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]"></div>
+                  <div className="flex justify-center -mt-28">
+                    <div className="relative inline-flex">
+                      <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)] group-hover:bg-zinc-950/30 transition duration-300"></div>
+                      <img
+                        src={image}
+                        className="size-40 group-hover:-translate-y-6 duration-300"
+                      />
+                    </div>
+                  </div>
+                  <h3 className="font-heading font-black text-3xl mt-12">
+                    {title}
+                  </h3>
+                  <p className="text-lg text-zinc-400 mt-4">{description}</p>
+                  <div className="flex justify-between mt-12">
+                    <button className={
+                      twMerge(
+                        'inline-flex items-center gap-2 text-lg font-semibold text-zinc-500 group-hover:text-zinc-300 transition duration-300',
+                        color === 'lime' && 'text-lime-500 group-hover:text-lime-300',
+                        color === 'cyan' && 'text-cyan-500 group-hover:text-cyan-300',
+                        color === 'violet' && 'text-violet-500 group-hover:text-violet-300',
+                      )
+                    }>
+                      Learn More
+                    </button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2}
+                      stroke="currentColor"
+                      className="size-8 text-zinc-500 group-hover:text-zinc-300 transition duration-300 -translate-x-2 group-hover:translate-x-0"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex items-center justify-center mt-10">
+          <div className="inline-flex bg-zinc-950 gap-4 p-2.5 rounded-full">
+            {data.map(({ title }) => {
+              return (
+                <div
+                  key={title}
+                  className="size-2.5 bg-zinc-500 rounded-full cursor-pointer  "
+                ></div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturesCards;
