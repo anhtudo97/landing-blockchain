a0fe0e08a426d0bbe021646c5bcc8a59b7a13d28
Alex Dou <anhtudo97@gmail.com>
2025-05-22 01:58:39 +0700

    feat: hero animation

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,15 +1,32 @@
-import React, { type ComponentPropsWithoutRef } from 'react';
+import { type ComponentPropsWithoutRef } from 'react';
 import { twMerge } from 'tailwind-merge';
+import { motion } from 'framer-motion';
 
-const Circle = ({ className, children }: ComponentPropsWithoutRef<'div'>) => {
+const Circle = ({ className, children, animate }: ComponentPropsWithoutRef<'div'> & {animate?: boolean}) => {
   return (
     <div
       className={twMerge(
-        'bg-zinc-900 size-[240px] inline-flex items-center justify-center rounded-full outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10',
-        'left-[200px] top-[270px]',
+        'bg-zinc-900 size-[240px] inline-flex items-center justify-center rounded-full',
+        'left-[200px] top-[270px] relative',
         className,
       )}
     >
+      <motion.div
+       className={
+        twMerge(
+          "absolute inset-0 outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10 border-[6px] border-transparent rounded-full",
+          animate && "border-t-fuchsia-500/30"
+        )
+       }
+       animate={animate &&{
+         rotate: 360, 
+       }}
+       transition={{
+         ease: 'linear',
+         repeat: Infinity,
+         duration: 15 ,
+       }}
+       />
       {children}
     </div>
   );
--- src/components/Hexagon.tsx
+++ src/components/Hexagon.tsx
@@ -1,24 +1,92 @@
-import type { ComponentPropsWithoutRef } from "react";
-import { twMerge } from "tailwind-merge";
+import {
+  useLayoutEffect,
+  useRef,
+  useState,
+  type ComponentPropsWithoutRef,
+} from 'react';
+import { twMerge } from 'tailwind-merge';
+import { motion } from 'framer-motion';
 
-const Hexagon = ({ className }: ComponentPropsWithoutRef<'svg'>) => {
-    return (
-        <svg
-            width="82"
-            height="72"
-            viewBox="0 0 82 72"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="6"
-            xmlns="http://www.w3.org/2000/svg"
-            className={twMerge("text-fuchsia-500/10 -rotate-6", className)}
+const Hexagon = ({
+  className,
+  size = 800,
+  reverse = false,
+  duration = 20,
+}: ComponentPropsWithoutRef<'svg'> & {
+  size?: number;
+  reverse?: boolean;
+  duration?: number;
+}) => {
+  const pathRef = useRef<SVGPathElement>(null);
+  const [totalPathLength, setTotalPathLength] = useState<number>();
+
+  useLayoutEffect(() => {
+    const path = pathRef.current;
+    if (!path) return;
+    const length = path.getTotalLength();
+    const scalePathLength = (length * size) / 82;
+    setTotalPathLength(scalePathLength);
+  }, []);
+  return (
+    <div className="inline-flex relative">
+      <svg
+        width="82"
+        height="72"
+        viewBox="0 0 82 72"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="6"
+        xmlns="http://www.w3.org/2000/svg"
+        className={twMerge('text-fuchsia-500/10 -rotate-6', className)}
+        style={{
+          height: size,
+          width: size,
+        }}
+      >
+        <path
+          d="M3.44337 38.5C2.55021 36.953 2.55021 35.047 3.44338 33.5L20.0566 4.72501C20.9498 3.178 22.6004 2.22501 24.3868 2.22501H57.6132C59.3996 2.22501 61.0502 3.178 61.9434 4.72501L78.5566 33.5C79.4498 35.047 79.4498 36.953 78.5566 38.5L61.9434 67.275C61.0502 68.822 59.3996 69.775 57.6132 69.775H24.3867C22.6004 69.775 20.9498 68.822 20.0566 67.275L3.44337 38.5Z"
+          vector-effect="non-scaling-stroke"
+          ref={pathRef}
+        />
+      </svg>
+      {totalPathLength && (
+        <motion.svg
+          width="82"
+          height="72"
+          viewBox="0 0 82 72"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="6"
+          xmlns="http://www.w3.org/2000/svg"
+          className={twMerge(
+            'text-fuchsia-500/30 -rotate-6 absolute',
+            className,
+          )}
+          initial={{
+            strokeDashoffset: '0',
+          }}
+          animate={{
+            strokeDashoffset: reverse ? totalPathLength : totalPathLength * -1,
+          }}
+          transition={{
+            repeat: Infinity,
+            duration,
+            ease: 'linear',
+          }}
+          style={{
+            strokeDasharray: `500 ${totalPathLength - 500}`,
+            height: size,
+            width: size,
+          }}
         >
-            <path
-                d="M3.44337 38.5C2.55021 36.953 2.55021 35.047 3.44338 33.5L20.0566 4.72501C20.9498 3.178 22.6004 2.22501 24.3868 2.22501H57.6132C59.3996 2.22501 61.0502 3.178 61.9434 4.72501L78.5566 33.5C79.4498 35.047 79.4498 36.953 78.5566 38.5L61.9434 67.275C61.0502 68.822 59.3996 69.775 57.6132 69.775H24.3867C22.6004 69.775 20.9498 68.822 20.0566 67.275L3.44337 38.5Z"
-                vector-effect="non-scaling-stroke"
-            />
-        </svg>
-    );
+          <path
+            d="M3.44337 38.5C2.55021 36.953 2.55021 35.047 3.44338 33.5L20.0566 4.72501C20.9498 3.178 22.6004 2.22501 24.3868 2.22501H57.6132C59.3996 2.22501 61.0502 3.178 61.9434 4.72501L78.5566 33.5C79.4498 35.047 79.4498 36.953 78.5566 38.5L61.9434 67.275C61.0502 68.822 59.3996 69.775 57.6132 69.775H24.3867C22.6004 69.775 20.9498 68.822 20.0566 67.275L3.44337 38.5Z"
+            vector-effect="non-scaling-stroke"
+          />
+        </motion.svg>
+      )}
+    </div>
+  );
 };
 
 export default Hexagon;
